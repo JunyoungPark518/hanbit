54de873dd58cfc2db24f282c8d8fb21aa93403a2
JunyoungPark518 <babungv@gmail.com>
2016-12-28 09:04:48 +0900

    Euclid++

--- src/com/hanbit/math/EuclideAlgorithm.java
+++ src/com/hanbit/math/EuclideAlgorithm.java
@@ -6,41 +6,26 @@ public class EuclideAlgorithm {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the integer number");
-		int number1 = sc.nextInt(), number2 = 0, quota = 0, remain = 0, lcm = 0, gcd = 0, temp1 = 0, temp2 = 0;
+		int number1 = sc.nextInt(), number2 = 0, remain = 0, lcm = 0, gcd = 0, temp1 = 0, temp2 = 0;
 		System.out.println("Enter the comparasion integer number ");
 		number2 = sc.nextInt();
-		temp1 = number1;
-		temp2 = number2;
-		if (number1 >= number2) {
-			while(true) {
-				quota = number1 / number2; 
-				remain = number1 % number2;
-				number1 = number2;
-				number2 = remain;
-				if(remain==0) {
-					gcd = number1;
-					break;
-				}
-			}
-			System.out.printf("Greatest Common Divisor:%d\n", gcd);
-			lcm = (int) (temp1 * temp2 / gcd);
-			System.out.printf("Least Common Multiple:%d", lcm);
+		if(number1 >= number2) {
+			temp1 = number1;
+			temp2 = number2;
 		} else {
-			number1 = temp2;
-			number2 = temp1;
-			while(true) {
-				quota = number1 / number2; 
-				remain = number1 % number2;
-				number1 = number2;
-				number2 = remain;
-				if(remain==0) {
-					gcd = number1;
-					break;
-				}
-			}
-			System.out.printf("Greatest Common Divisor:%d\n", gcd);
-			lcm = (int) (temp1 * temp2 / gcd);
-			System.out.printf("Least Common Multiple:%d", lcm);
+			temp2 = number1;
+			temp1 = number2;
 		}
+		while (true) {
+			remain = temp1 % temp2;
+			temp1 = temp2;
+			temp2 = remain;
+			if (remain == 0) {
+				gcd = temp1;
+				break;
+			}
+		}
+		lcm = (int) (number1 * number2 / gcd);
+		System.out.printf("Greatest Common Divisor:%d\nLeast Common Multiple:%d", gcd, lcm);
 	}
 }
