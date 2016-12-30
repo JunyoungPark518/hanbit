2d2b666ccf4481cf4fe388f12c99a63be933d320
JunyoungPark518 <babungv@gmail.com>
2016-12-30 12:19:42 +0900

    add scanner

--- src/com/hanbit/matrix/Diamond.java
+++ src/com/hanbit/matrix/Diamond.java
@@ -1,4 +1,7 @@
 package com.hanbit.matrix;
+
+import java.util.Scanner;
+
 /*
 0    0    1    0    0   
 0    2    3    4    0   
@@ -8,17 +11,25 @@ package com.hanbit.matrix;
  * */
 public class Diamond {
     public static void main(String[] args) {
-        int len=5, col= len, row= len, val=0, start=0, end=0; //배열에 저장할 변수
+    	Scanner sc = new Scanner(System.in);
+        int len=0;
+        while(true) {
+        	if(len%2!=0) {
+        		break;
+        	} else {
+        		System.out.print("Insert odd number:");
+        		len = sc.nextInt();
+        	}
+        }
+        int col=len, row=len, val=0, start=0, end=0, fixed=3; //배열에 저장할 변수
         int[][] arr = new int[ col][ row];
-        start=3; //열의 시작 위치
-        end=3; //열의 끝 위치
+        start=end=len/2; //열의 끝 위치
         for(int i=0; i<col; i++) {
         	for(int j=0; j<row; j++) {
-        		arr[i][j] = val;
-        		System.out.printf("%d\t",val);
+        		arr[i][j] = (start<=i+j && end*fixed>=i+j && i>=j-start && j>=i-end) ? ++val : 0;
+        		System.out.printf("%d\t",arr[i][j]);
         	}
         	System.out.println();
         }
-
     }
 }
--- src/com/hanbit/matrix/Zigzag.java
+++ src/com/hanbit/matrix/Zigzag.java
@@ -1,8 +1,12 @@
 package com.hanbit.matrix;
 
+import java.util.Scanner;
+
 public class Zigzag {
     public static void main(String[] args) {
-        int length = 5; // 배열 길이
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Insert the number:");
+        int length = sc.nextInt(); // 배열 길이
         int[][] arr = new int[length][length];
         int k = 1; // 넣을 값
         int flag = 1; // 스위치변수
@@ -10,13 +14,12 @@ public class Zigzag {
         int j = 0; // 열
         for(i=0;i<arr.length;i++) {
     		for(j=0; j<arr[i].length; j++) {
-    			arr[i][j] = k;
+    			flag = (i%2==0) ? 1 : -1;
+    			arr[i][j] = (i%2==0) ? i*length+k : i*length+flag*j+length;
     			k++;
     			System.out.printf("%d\t",arr[i][j]);
-    			if(i==1||i==3) {
-    				
-    			}
     		}
+    		k-=length;
         	System.out.println();
         }
 
